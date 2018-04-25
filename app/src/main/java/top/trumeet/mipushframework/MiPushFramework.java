89570412e0bfa561e532abdf4ca0c4b9c95d2500
wzv5 <wzv5@msn.com>
2018-04-25 22:28:04 +0800

    x

--- app/src/main/java/top/trumeet/mipushframework/MiPushFramework.java
+++ app/src/main/java/top/trumeet/mipushframework/MiPushFramework.java
@@ -3,10 +3,8 @@ package top.trumeet.mipushframework;
 import android.app.Application;
 import android.content.SharedPreferences;
 
-import com.crashlytics.android.Crashlytics;
 import com.crossbowffs.remotepreferences.RemotePreferenceAccessException;
 
-import io.fabric.sdk.android.Fabric;
 import top.trumeet.common.utils.PreferencesUtils;
 import top.trumeet.mipush.BuildConfig;
 import top.trumeet.mipushframework.utils.BaseAppsBinder;
@@ -19,13 +17,6 @@ public class MiPushFramework extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (!BuildConfig.DEBUG) {
-            final Fabric fabric = new Fabric.Builder(this)
-                    .kits(new Crashlytics())
-                    .build();
-            Fabric.with(fabric);
-        }
-
         {
             //debugIcon init
             try {
